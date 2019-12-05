54a229737073da44b9079a14f3c41ad47a86f5e4
Niklas Mevenkamp <niklas.mevenkamp@gmail.com>
2019-12-05 12:57:17 +0100

    add force refresh batch script

---
+++ force_refresh.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+:: Run wallpaper refresh via powershell
+CALL :NORMALIZEPATH "%0\..\powershell_scripts\refresh_wallpaper.ps1"
+SET PS_REFRESH_WP_PATH=%RETVAL%
+powershell -ExecutionPolicy ByPass -File %PS_REFRESH_WP_PATH%
+
+
+ECHO.
+ECHO Wallpaper has been refreshed.
+ECHO.
+ECHO.
+SET /p FOO="Press any key to continue..."
+
+
+:: ========== FUNCTIONS ==========
+EXIT /B
+
+:NORMALIZEPATH
+  SET RETVAL=%~dpfn1
+  EXIT /B
