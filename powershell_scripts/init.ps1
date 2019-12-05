24b36856275c2bf115db5c560a842c40ed40dfd5
Niklas Mevenkamp <niklas.mevenkamp@gmail.com>
2019-12-05 13:20:43 +0100

    add proper prompt for wallpaper directory

--- powershell_scripts/init.ps1
+++ powershell_scripts/init.ps1
@@ -1,12 +1,25 @@
-﻿# Parse command line arguments
-#param(
-#    [Parameter(Mandatory=$true)][string]$wallpaper_base_dir
-#)
-$wallpaper_base_dir = "C:\MicroSD\Pictures\Desktop Wallpapers" 
-
-# Load WallpaperScheduler module
+﻿# Load WallpaperScheduler module
 $script_dir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . "$script_dir\wallpaper_scheduler.ps1"
+
+# Parse command line arguments
+$input_valid = $false
+while (-Not $input_valid) {
+    $wallpaper_base_dir = Read-Host -Prompt "Specify your desired wallpaper base directory"
+    
+    # remove enclosing quotes in case the user specified such
+    $wallpaper_base_dir = $wallpaper_base_dir.Replace("'", "").Replace('"', '')
+
+    try {
+        New-Item -ItemType Directory -Force -Path $wallpaper_base_dir | Out-Null
+
+        if ($(Test-Path $wallpaper_base_Dir -PathType Container)) {
+            $input_valid = $true
+        }
+    } catch {
+        $input_valid = $false
+    }
+}
 
 # Invoke init
 Wallpaper-Scheduler-Init $wallpaper_base_dir
